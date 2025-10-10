cc90da8924816c099672f09ed487f5f41e86aad2
Zach Barnett <zacharybarnett96@gmail.com>
2025-10-09 19:15:52 -0700

    Rough outlining files

---
+++ README.md
@@ -0,0 +1,6 @@
+# BYU-Pathway WDD330 Final Project
+## My Local Events
+
+### Author: Zach Barnett
+
+This repository is the code for my final project, a webpage that determine's the visitor's rough location based on their IP and displays events for that area.
---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { ressolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+	root: "src/",
+
+	build: {
+		outDir: "../dist",
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "src/index.html"),
+				starred: resolve(__dirname, "src/starred/index.html"),
+			},
+		},
+	},
+});
